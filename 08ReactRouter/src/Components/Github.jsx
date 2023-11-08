e4ba6e8026fc18f22ef52545af48a2769fd78005
Shahidnadaf7348888 <shahidnadaf2673@gmail.com>
2023-11-08 14:58:38 +0530

    Update

--- 08ReactRouter/src/Components/Github.jsx
+++ 08ReactRouter/src/Components/Github.jsx
@@ -2,18 +2,7 @@
 import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-
   const data = useLoaderData()
-
-  // const [data, setData]= useState([])
-  //   useEffect(()=>{
-  //     fetch('https://api.github.com/users/Shahidnadaf7348888')
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       console.log(data)
-  //       setData(data)})
-  //   },[])
-
   return (
     <>
           <div className="flex justify-center p-12 ">
